290129f0e8cf833cd3dd6ea8947b32f4167549ec
evenmn <evenmn@fys.uio.no>
2020-10-15 22:29:51 +0200

    Added Ewald parameterization of TIP4P

---
+++ genpot/param/tip4p/tip4p-ew.py
@@ -0,0 +1,14 @@
+# Horn, Swope, Pitera, J. Chem. Phys. 120, 9665 (2004)
+
+citation = "Horn, Swope, Pitera, J. Chem. Phys. 120, 9665 (2004)"
+
+molecule = "HOH"
+
+params = {"Z_H": 0.5242,
+          "Z_O": -1.0484,
+          "r0": 0.9572,
+          "theta": 104.52,
+          "OM": 0.1250,
+          "epsilon": 0.16275,
+          "sigma": 3.16435,
+          "r4s": 20}
