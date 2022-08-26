707b84bab82017853edfb9c7c27a00516ac02adc
evenmn <evenmn@fys.uio.no>
2022-08-26 20:36:14 +0200

    '__call__'-method now calls the 'write'-method

--- genpot/forcefield.py
+++ genpot/forcefield.py
@@ -3,7 +3,6 @@ import re
 import shutil
 import datetime
 import importlib
-import math
 import copy
 
 
@@ -190,19 +189,4 @@ class ForceField:
         :param params: dictionary of parameters that should be changed
         :type params: dict
         """
-        # Make new parameter file
-        this_dir, this_filename = os.path.split(__file__)
-        header_filename = os.path.join(this_dir, f"data/header.{self.__repr__()}")
-
-        shutil.copyfile(header_filename, filename)
-        with open(filename, 'r+') as f:
-            content = f.read()
-            f.seek(0, 0)
-            f.write(self.header.rstrip('\r\n') + '\n' + content)
-            for suffix_list in self.suffices:
-                f.write("#" + 11 * " " + ", ".join(suffix_list) + "\n")
-
-        # Add parameters to file
-        for group, params in self.params.items():
-            self.append_type_to_file(group, params, filename)
-        print(f"New parameter file '{filename}' successfully generated!")
+        self.write(filename)
--- genpot/vashishta.py
+++ genpot/vashishta.py
@@ -1,3 +1,4 @@
+import math
 from .forcefield import ForceField
 
 class Vashishta(ForceField):
