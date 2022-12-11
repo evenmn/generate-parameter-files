afb7de0ebb60392beb207281c41aaf414b4e3a5f
evenmn <evenmn@fys.uio.no>
2022-12-11 16:37:52 +0100

    Added Vink 2001 potential

---
+++ genpot/param/sw/sio2_vink_2001.py
@@ -0,0 +1,17 @@
+# Vink et al., Journal of Non-Crystalline Solids, v. 282, p. 248, (2001)
+
+citation = "Journal of Non-Crystalline Solids, v. 282, p. 248, (2001)"
+
+molecule = "OSiO"
+
+params = {"SiSiSi":    {"epsilon": 1.64833,
+                        "sigma": 2.0951,
+                        "a": 1.80,
+                        "lambda": 31.5,
+                        "gamma": 1.20,
+                        "cos(theta)": -0.33333333333,
+                        "A": 7.049556277,
+                        "B": 0.6022245584,
+                        "p": 4.0,
+                        "q": 0.0,
+                        "tol": 0.0}}
